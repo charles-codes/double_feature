a8bf5316b46ec9cae4fe36c1b7e2465bdf546e5a
Charles Tuggle <tuggle.charles@gmail.com>
2020-04-19 15:06:49 -0600

    working on saving search field values into localStorage

--- app_test.js
+++ app_test.js
@@ -2,12 +2,15 @@
 $("#add-df").on("click", function(event) {
   event.preventDefault();
 
+  // variables for capturing movie titles
   var movie_1 = $("#movie-input-1").val().trim();
   var movie_2 = $("#movie-input-2").val().trim();
 
+  // variables for the API queries
   var queryURL = "http://www.omdbapi.com/?i=tt3896198&t=" + movie_1 + "&apikey=7d0feada";
   var queryURL2 = "http://www.omdbapi.com/?i=tt3896198&t=" + movie_2 + "&apikey=7d0feada";
 
+  // ajax call for movie_1
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -36,6 +39,7 @@ $("#add-df").on("click", function(event) {
     $("#movies-view-1").append(movieDiv);
   })
 
+  // ajax call for movie 2
   $.ajax({
     url: queryURL2,
     method: "GET"
@@ -80,9 +84,28 @@ $("#add-df").on("click", function(event) {
 
   $("#df-blurb-view").append(blurbP);
 
+  // set localStorage
+  localStorage.setItem("movie 1", movie_1);
+  localStorage.setItem("movie 2", movie_2);
+  localStorage.setItem("df title", title);
+  localStorage.setItem("df blurb", blurb);
+
   // clear input text fields
   $("#movie-input-1").val("");
   $("#movie-input-2").val("");
   $("#df-title-input").val("");
   $("#df-blurb-input").val("");
 });
+
+// function for get items from localStorage and displaying them in search fields
+function getSearchFields() {
+  var savedMovie1 = localStorage.getItem("movie 1");
+  var savedMovie2 = localStorage.getItem("movie 2");
+  var savedTitle = localStorage.getItem("df title");
+  var savedBlurb = localStorage.getItem("df blurb");
+
+  $("#movie-input-1").val(savedMovie1);
+  $("#movie-input-2").val(savedMovie2);
+  $("#df-title-input").val(savedTitle);
+  $("#df-blurb-input").val(savedBlurb);
+}
